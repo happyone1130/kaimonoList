f84eb7fbc6f2f3bc31400eaf2986ebcd8b73d02f
happyone1130 <aukamisaikyou1130@gmail.com>
2020-06-11 11:09:55 +0900

    Initial Commit

---
+++ kaimonoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kaimonoList
+//
+//  Created by 伊藤滉佑 on 2020/06/11.
+//  Copyright © 2020 Ito Kosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
